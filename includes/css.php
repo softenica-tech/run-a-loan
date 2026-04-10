88b7b20a1c1d59345cdf513ca6c0d9f053f4ddf7
Asad Rehman <softenica@gmail.com>
2026-04-10 15:10:47 +1000

    URL Updates

--- includes/css.php
+++ includes/css.php
@@ -1,5 +1,14 @@
+<?php
+$scriptName = $_SERVER['SCRIPT_NAME'] ?? '/';
+$basePath = rtrim(str_replace('\\', '/', dirname($scriptName)), '/');
+
+if ($basePath === '' || $basePath === '.') {
+    $basePath = '';
+}
+?>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" > 
+<base href="<?php echo htmlspecialchars($basePath . '/'); ?>">
 <link rel="shortcut icon" href="favicon.png" type="image/png"> 
 <!-- Bootstrap 5 -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
