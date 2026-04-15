84adba2c23ed97c8dcc5e550f4a0d5916e6cbddf
softenica-tech <softenica.cs@gmail.com>
2026-04-15 17:07:32 +0500

    meta info and bottom to top button added

---
+++ js/main.js
@@ -0,0 +1,18 @@
+ // Back to Top 
+const backToTop = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 500) {
+        backToTop.classList.add("show");
+    } else {
+        backToTop.classList.remove("show");
+    }
+});
+
+backToTop.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
+// Back to Top 
